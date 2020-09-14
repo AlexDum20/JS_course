1b11fe9080e7c527f4d79d4957ed3ccb9217819a
AlexDum20 <dumchevas@yahoo.com>
2020-09-14 12:29:04 +0300

    changing the HTML page

--- 1.js
+++ 1.js
@@ -19,7 +19,7 @@ const personalMovieDB = {
             const a = prompt('Один из последних просмотренных фильмов?', ''),
                 b = +prompt('На сколько оцените его?', '');
 
-            if (a != null && b != null && a != '' && b != '' && a.length < 50 && b != NaN) {
+            if (a != null && b !== null && a != '' && b !== '' && a.length < 50 && b !== NaN) {
                 personalMovieDB.movies[a] = b;
                 console.log('done');
             } else {
